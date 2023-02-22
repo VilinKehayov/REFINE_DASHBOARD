db6d0c9e731362ebbf365b11dbb84cd3acd870af
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-02-22 08:47:00 +0200

    Started to build home component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,48 @@
-import {useList} from '@pankod/refine-core';
+import { useList } from "@pankod/refine-core";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent,
+} from "components";
 
-const  home = () => {
+const home = () => {
   return (
-    <div> home</div>
-  )
-}
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
 
-export default  home
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart 
+        title='Properties for Sale'
+        value={684}
+        series={[75, 25]}
+        colors={['#475be8', '#e4e8ef']}
+        />
+        <PieChart 
+        title='Properties for Rent'
+        value={550}
+        series={[60, 40]}
+        colors={['#475ae8', '#e4b8ef']}
+        />
+        <PieChart 
+        title='Total customers'
+        value={5684}
+        series={[75, 25]}
+        colors={['#275be8', '#c4e8ef']}
+        />
+        <PieChart 
+        title='Properties for Cities'
+        value={555}
+        series={[75, 25]}
+        colors={['#475be8', '#e4e8ef']}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default home;
