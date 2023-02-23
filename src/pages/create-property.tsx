c441e21eaf5d1978e96519c393753963ec980d6d
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-02-23 11:15:57 +0200

    Started with createProperty component

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import {useState} from 'react'
+import {useGetIdentity} from '@pankod/refine-core'
+import {FieldValues, useForm} from '@pankod/refine-react-hook-form'
+import {useNavigate} from '@pankod/refine-react-router-v6'
+
 
 const createProperty = () => {
   return (
