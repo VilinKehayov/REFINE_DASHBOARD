eb3d46be57becf0648377a586d110d200abf3be5
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-01 15:47:44 +0200

    useTable hook implemented

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,11 +1,18 @@
 import { Add } from "@mui/icons-material";
-import { useList } from "@pankod/refine-core";
+import { useTable } from "@pankod/refine-core";
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
 import { PropertyCard, CustomButton } from "components";
 
 const AllProperties = () => {
   const navigate = useNavigate();
+  const {
+    tableQueryResult: { data, isLoading, isError },
+  } = useTable();
+  const allProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading....</Typography>;
+  if (isError) return <Typography>Error...</Typography>;
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
@@ -21,6 +28,11 @@ const AllProperties = () => {
           icon={<Add />}
         />
       </Stack>
+      <Box mt="20px" sx={{display: "flex", flexWrap: "wrap", gap: 3}}>
+        {allProperties.map((property)=>(
+          <PropertyCard />
+        ))};
+      </Box>
     </Box>
   );
 };
