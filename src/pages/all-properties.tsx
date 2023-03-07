ac1e497b855e4eb1d9d563e0a475218e073c3f14
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-07 11:53:27 +0200

    All properties top page done

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,6 +1,13 @@
 import { Add } from "@mui/icons-material";
 import { useTable } from "@pankod/refine-core";
-import { Box, Stack, Typography } from "@pankod/refine-mui";
+import {
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+} from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
 import { PropertyCard, CustomButton } from "components";
 
@@ -8,18 +15,74 @@ const AllProperties = () => {
   const navigate = useNavigate();
   const {
     tableQueryResult: { data, isLoading, isError },
+    current,
+    setCurrent,
+    setPageSize,
+    pageCount,
+    sorter,
+    setSorter,
+    filter,
+    setFilters,
   } = useTable();
+  console.log(data);
   const allProperties = data?.data ?? [];
 
   if (isLoading) return <Typography>Loading....</Typography>;
   if (isError) return <Typography>Error...</Typography>;
   return (
     <Box>
-      <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Typography fontSize={25} fontWeight={700} color="#11142d">
-          All Properties
-        </Typography>
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        <Stack direction="column" width="100%">
+          <Typography fontSize={25} fontWeight={700} color="#11142d">
+            {!allProperties.length
+              ? "There are no properties"
+              : "All Properties"}
+          </Typography>
+          <Box
+            mb={2}
+            mt={3}
+            display="flex"
+            width="84%"
+            justifyContent="space-between"
+            flexWrap="wrap"
+          >
+            <Box
+              display="flex"
+              gap={2}
+              flexWrap="wrap"
+              mb={{ xs: "20px", sm: 0 }}
+            >
+              <CustomButton
+                title={`Sort price`}
+                handleClick={() => {}}
+                backgroundColor="#475be8"
+                color="#fcfcfc"
+              />
+              <TextField
+                variant="outlined"
+                color="info"
+                placeholder="Search by title"
+                value=""
+                onChange={() => {}}
+              />
+              <Select
+                variant="outlined"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ "aria-label": "Without label" }}
+                defaultValue=""
+                value=""
+                onChange={() => {}}
+              >
+                <MenuItem value="">All</MenuItem>
+              </Select>
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
 
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
         <CustomButton
           title="Add Property"
           handleClick={() => navigate("/properties/create")}
@@ -28,18 +91,20 @@ const AllProperties = () => {
           icon={<Add />}
         />
       </Stack>
-      <Box mt="20px" sx={{display: "flex", flexWrap: "wrap", gap: 3}}>
-        {allProperties.map((property)=>(
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        {allProperties.map((property) => (
           <PropertyCard
-          key={property._id}
-          id={property._id}
-          title={property.title}
-          price={property.price}
-          location={property.location}
-          photo={property.photo}
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            price={property.price}
+            location={property.location}
+            photo={property.photo}
           />
-        ))};
+        ))}
+        ;
       </Box>
+      
     </Box>
   );
 };
