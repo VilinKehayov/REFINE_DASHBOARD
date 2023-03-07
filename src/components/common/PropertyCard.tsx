e7b404a37c3654bfd8ad35e0479c3b4e47131aaa
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-07 08:57:23 +0200

    PropertyCard component done

--- src/components/common/PropertyCard.tsx
+++ src/components/common/PropertyCard.tsx
@@ -29,6 +29,7 @@ const PropertyCard = ({
           boxShadow: "0 22px 45px 2px rgba(176, 176, 176, 0.1)",
         },
         cursor: "pointer",
+        textDecoration: "none"
       }}
       elevation={0}
     >
@@ -40,6 +41,24 @@ const PropertyCard = ({
         alt="card image"
         sx={{ borderRadius: "10px" }}
       />
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Stack direction="column" gap={1}>
+          <Typography fontSize={16} fontWeight={500} color="#11142d">{title}</Typography>
+          <Stack direction="row" gap={0.5} alignItems="flex-start">
+            <Place sx={{ fontSize: 18, color: "#11142d", marginTop: 0.5 }} />
+            <Typography fontSize={14} color="#808191">{location}</Typography>
+          </Stack>
+        </Stack>
+        <Box px={1.5} py={0.5} borderRadius={1} bgcolor="#dadefa">
+          <Typography fontSize={12} fontWeight={600} color="#475be8">{price}</Typography>
+        </Box>
+      </CardContent>
     </Card>
   );
 };
