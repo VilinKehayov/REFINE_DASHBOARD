cfb71ce2908ff67cfcd2297b95f3ddbd0a61dda8
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-09 08:51:44 +0200

    PropertyDetails component still building...

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -1,22 +1,52 @@
-import {Typography, Box, Stack} from "@pankod/refine-mui"
-import {useDelete, useGetIdentity, useShow} from "@pankod/refine-core"
-import {useParams, useNavigate} from "@pankod/refine-react-router-v6"
-import {ChatBubble, Delete, Edit, Phone, Place, Star} from "@mui/icons-material"
+import { Typography, Box, Stack } from "@pankod/refine-mui";
+import { useDelete, useGetIdentity, useShow } from "@pankod/refine-core";
+import { useParams, useNavigate } from "@pankod/refine-react-router-v6";
+import {
+  ChatBubble,
+  Delete,
+  Edit,
+  Phone,
+  Place,
+  Star,
+} from "@mui/icons-material";
 
-import {CustomButton} from "components"
+import { CustomButton } from "components";
 
 const PropertyDetails = () => {
   const navigate = useNavigate();
-  const {data: user} = useGetIdentity();
-  const {id} = useParams();
-  const {mutate} = useDelete();
-  const {queryResult} = useShow();
+  const { data: user } = useGetIdentity();
+  const { id } = useParams();
+  const { mutate } = useDelete();
+  const { queryResult } = useShow();
 
-  const {data, isLoading, isError} = queryResult;
-  
+  const { data, isLoading, isError } = queryResult;
+
+  const propertyDetails = data?.data ?? {};
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+
   return (
-    <div>propertyDetails</div>
-  )
-}
+    <Box
+      borderRadius="15px"
+      padding="20px"
+      bgcolor="#fcfcfc"
+      width="fit-content"
+    >
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Details
+      </Typography>
+      <Box
+        mt="20px"
+        display="flex"
+        flexDirection={{ xs: colum, lg: "row" }}
+        gap={4}
+      >
+        
 
-export default PropertyDetails
+      </Box>
+    </Box>
+  );
+};
+
+export default PropertyDetails;
