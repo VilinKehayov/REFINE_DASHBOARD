8d806d171892bfb153947a738144a263b0440b09
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-15 16:37:31 +0200

    Started with AgentCard component..Part of Agent pg

--- src/components/agent/AgentCard.tsx
+++ src/components/agent/AgentCard.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {AgentCardProp, InfoBarProps} from "interfaces/agent"
+import {EmailOutlined, LocationCity, Phone, Place} from "@mui/icons-material"
+import {useGetIdentity} from "@pankod/refine-core";
+import {Box, Stack, Typography} from "@pankod/refine-mui"
+import {Link} from "@pankod/refine-react-router-v6"
 
-const AgentCard = () => {
+
+const AgentCard = ({id, name, email, avatar, noOfProperties}: AgentCardProp) => {
+  const {data: currentUser} = useGetIdentity();
+
+  const generateLink = () => {
+    if(currentUser.email === email) return "/my-profile"
+
+    return `agent/show/${id}`
+  }
+ 
   return (
-    <div>AgentCard</div>
+    <Box
+    component={Link}
+    to={generateLink()}
+    >
+      AgentCard
+    </Box>
   )
 }
 
--- src/pages/agent.tsx
+++ src/pages/agent.tsx
@@ -1,17 +1,45 @@
-import {useList} from "@pankod/refine-core";
-import {Box, Typography} from "@pankod/refine-mui";
+import { useList } from "@pankod/refine-core";
+import { Box, Typography } from "@pankod/refine-mui";
 
-import {AgentCard} from "components"
+import { AgentCard } from "components";
 
 const Agents = () => {
-  const {data, isLoading, isError} = useList({
-    resource: "user",
+  const { data, isLoading, isError } = useList({
+    resource: "api/v1/users",
   });
-  
-  const allAgents = data?.data;
-  return (
-    <div>agent</div>
-  )
-}
 
-export default Agents
+  const allAgents = data?.data ?? [];
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Agents List
+      </Typography>
+      <Box
+        mt="20px"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          backgroundColor: "#fcfcfc",
+        }}
+      >
+        {allAgents.map((agent) => (
+          <AgentCard
+            key={agent._id}
+            id={agent._id}
+            name={agent.name}
+            email={agent.email}
+            avatar={agent.avatar}
+            noOfProperties={agent.allProperties.length}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default Agents;
