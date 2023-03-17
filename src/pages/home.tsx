1fd60a4e133f96ca25615104af7b29eb3136b8a3
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-17 11:38:24 +0200

    displaying properties on home page..

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,21 @@ import {
   TopAgent,
 } from "components";
 
-const home = () => {
+const Home = () => {
+  const {data, isLoading, isError} = useList({
+    resource: "api/v1/properties",
+    config: {
+      pagination: {
+        pageSize: 5
+      }
+    }
+  })
+
+  const latestProperties = data?.data ?? [];
+
+  if(isLoading) return <Typography>Loading...</Typography>
+  if(isError) return <Typography>Error...</Typography>
+
   return (
     <Box>
       <Typography fontSize={25} fontWeight={700} color="#11142D">
@@ -51,8 +65,38 @@ const home = () => {
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
+
+      <Box
+        flex={1}
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fcfcfc"
+        display="flex"
+        flexDirection="column"
+        minWidth="100%"
+        mt="25px"
+      >
+        <Typography
+          fontSize="18px"
+          fontWeight={600}
+          color="#11142d"
+        ></Typography>
+
+        <Box mt={2.5} sx={{ display: "flex", flexWrap: "wrap", gap: 4 }}>
+          {latestProperties.map((property)=> (
+            <PropertyCard 
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            location={property.location}
+            price={property.price}
+            photo={property.photo}
+            />
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-export default home;
+export default Home;
